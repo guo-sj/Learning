3954fd9cb0065dea4c8639a0375dd1ce419af661
jackie <515878133@qq.com>
2021-04-21 17:32:36 +0800

    1 update rfcomm-client.c 2 add l2cap protocol server && client source file

--- Technology/Bluetooth/BluetoothC/l2cap-client.c
+++ Technology/Bluetooth/BluetoothC/l2cap-client.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <bluetooth/bluetooth.h>
-#include <bluetooth/rfcomm.h>
+#include <bluetooth/l2cap.h>
 
 int main(int argc, char **argv)
 {
-    struct sockaddr_rc addr = { 0 };
+    struct sockaddr_l2 addr = { 0 };
     int s, status;
-    char dest[18] = "01:23:45:67:89:AB";
     char message[100] = "";
+    char dest[18] = "01:23:45:67:89:AB";
 
     if (argc < 3) {
         fprintf(stderr, "usage: %s <bt_addr> <message>\n", argv[0]);
@@ -21,22 +22,20 @@ int main(int argc, char **argv)
     strncpy(message, argv[2], 100);
 
     /* allocate a socket */
-    s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+    s = socket(AF_BLUETOOTH, SOCK_SEQPACKET, BTPROTO_L2CAP);
 
-    /* set the connection parameters (who to connect to) */
-    addr.rc_family = AF_BLUETOOTH;
-    addr.rc_channel = (uint8_t) 1;
-    str2ba(dest, &addr.rc_bdaddr);
+    /* set the connection parameters */
+    addr.l2_family = AF_BLUETOOTH;
+    addr.l2_psm = htobs(0x1001);
+    str2ba(dest, &addr.l2_bdaddr);
 
     /* connect to server */
     status = connect(s, (struct sockaddr *)&addr, sizeof(addr));
 
-    /* send a message */
     if (status == 0)
         status = write(s, message, sizeof(message));
 
-    if (status < 0) perror("uh oh");
+    if (status < 0)  perror("uh oh");
 
     close(s);
-    return 0;
 }
